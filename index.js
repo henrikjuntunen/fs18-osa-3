94d54a8cb17f08f77d0eec8b941fce53e736b8e4
Henrik Juntunen <henrik.juntunen@pp.inet.fi>
2018-10-17 19:52:39 +0300

    mooc fullstack osa 3

---
+++ index.js
@@ -0,0 +1,29 @@
+// node puhelinluettelo
+
+
+
+
+
+let notes = [
+    {
+        name: 'Maija Kannisto',
+        number: '04055512345',
+        id: 1
+    },
+    {
+        name: 'Kalevi Kannisto',
+        number: '04055522345',
+        id: 2
+    },
+    {
+        name: 'Pihla Kannisto',
+        number: '04055532345',
+        id: 3
+    },
+    {
+        name: 'Risto Kannisto',
+        number: '04055542345',
+        id: 4
+    }
+  ]
+
