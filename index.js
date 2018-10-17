dbb6c390415c0465ae65c972473bef5fbac85582
Henrik Juntunen <henrik.juntunen@pp.inet.fi>
2018-10-17 20:39:27 +0300

    mooc fullstack osa 3

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // index.js node puhelinluettelo on server
+// 3.1 puhelinluettelon backend osa 1 expressin alkeet
+// 3.2 puhelinluettelon backend osa 2
+// 3.3 puhelinluettelon backend osa 3
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -25,16 +28,38 @@ let persons = [
         name: 'Risto Kannisto',
         number: '04055542345',
         id: 4
+    },
+    {
+        name: 'Raija Kannisto',
+        number: '04055542345',
+        id: 5
     }
   ]
 
-  app.get('/', (req, res) => {
+  // 3.2 puhelinluettelon backend osa 2
+  app.get('/info', (req, res) => {
+      let cDate = new Date();
     console.log("req.url", req.url, "req.method", req.method)
-    res.send('<h1>Hello there World on port the 3003!</h1>')
+    res.send('<p class=\"info\">Puhelinluettelossa on ' + persons.length + 
+    ' henkilön tiedot ' + '<br>' + cDate + '<p>')
   })
 
+  // 3.1 puhelinluettelon backend osa 1
   app.get('/api/persons', (req, res) => {
     res.json(persons)
+  })
+
+  // 3.3 puhelinluettelon backend osa 3
+  app.get('/api/persons/:id', (request, response) => {
+    console.log("req.url", request.url, "req.method", request.method)
+    const id = Number(request.params.id)
+    const result = persons.find(x => x.id === id)
+  
+    if ( result ) {
+      response.json(result)
+    } else {
+      response.status(404).end()
+    }
   })
 
   const PORT = 3003
@@ -42,4 +67,15 @@ let persons = [
     console.log(`Server running on port ${PORT}`)
   })
   
+  /*
+  3.2 ja 3.3
+  TODO
+  app.js:22 Uncaught TypeError: Cannot read property 'innerHTML' of null
+    at Object.init (app.js:22)
+    at String.4 (app.js:112)
+    at _kitty_require (app.js:11)
+    at 4 (app.js:14)
+    at app.js:15
+  */
+
   // end index.js
