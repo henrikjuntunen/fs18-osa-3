b58880f6efb0f77503cc87989fc8b2d5b0c24392
Henrik Juntunen <henrik.juntunen@pp.inet.fi>
2018-10-17 20:14:11 +0300

    mooc fullstack osa 3

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// node puhelinluettelo
+// index.js node puhelinluettelo on server
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.json())
 
-
-
-
-let notes = [
+let persons = [
     {
         name: 'Maija Kannisto',
         number: '04055512345',
@@ -27,3 +28,18 @@ let notes = [
     }
   ]
 
+  app.get('/', (req, res) => {
+    console.log("req.url", req.url, "req.method", req.method)
+    res.send('<h1>Hello there World on port the 3003!</h1>')
+  })
+
+  app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
+
+  const PORT = 3003
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+  
+  // end index.js
