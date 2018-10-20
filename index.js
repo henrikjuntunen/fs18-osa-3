8ce1573b6841acb8146eaa8d2902e64f39bff34a
Henrik Juntunen <henrik.juntunen@pp.inet.fi>
2018-10-21 02:12:04 +0300

    mooc fullstack osa 3

--- index.js
+++ index.js
@@ -136,11 +136,11 @@ app.listen(PORT, () => {
 
 const PORT = process.env.PORT || 5000
 express()
-.use(express.static(path.join(__dirname, 'public')))
-// .set('views', path.join(__dirname, 'views'))
-// .set('view engine', 'ejs')
-//  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+//.use(express.static(path.join(__dirname, 'public')))
+//.set('views', path.join(__dirname, 'views'))
+//.set('view engine', 'ejs')
+//.get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT } having persons count `, persons.length))
 
 
   // end index.js
