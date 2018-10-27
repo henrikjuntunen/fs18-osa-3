9ddf4ba329b1f0adfad689ef86ab22428f90c5fd
Henrik Juntunen <henrik.juntunen@pp.inet.fi>
2018-10-27 10:08:13 +0300

    mooc fullstack osa 3

--- mongo.js
+++ mongo.js
@@ -31,13 +31,7 @@
 
 const mongoose = require('mongoose')
 
-// korvaa url oman tietokantasi urlilla. ethän laita salasanaa Githubiin!
-// const url = 'mongodb://fullstack:sekred@ds211088.mlab.com:11088/fullstack-notes'
-// mongodb://<dbuser>:<dbpassword>@ds245478.mlab.com:45478/hjperson
-// read only permission user
-// const url = 'mongodb://risto:p2assristo@ds245478.mlab.com:45478/hjperson'
-// update also permissioin user
-const url = 'mongodb://raija:p2assraija@ds245478.mlab.com:45478/hjperson'
+const url = 'mongodb'
 
 mongoose.connect(url)
 
