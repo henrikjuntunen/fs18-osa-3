23e5d3901f69b7bb1282acfb66183516c9e05db8
Henrik Juntunen <henrik.juntunen@pp.inet.fi>
2018-11-07 21:10:52 +0200

    3.22

--- models/person.js
+++ models/person.js
@@ -1,12 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
-// korvaa url oman tietokantasi urlilla. ethän laita salasanaa Githubiin!
-// const url = 'mongodb://fullstack:sekred@ds211088.mlab.com:11088/fullstack-notes'
-// mongodb://<dbuser>:<dbpassword>@ds245478.mlab.com:45478/hjperson
-// read only permission user
-// const url = 'mongodb://risto:p2assristo@ds245478.mlab.com:45478/hjperson'
-// update also permissioin user
 
 if ( process.env.NODE_ENV !== 'production' ) {
   require('dotenv').config()
@@ -14,7 +8,7 @@ if ( process.env.NODE_ENV !== 'production' ) {
 
 
 const url = process.env.MONGODB_URI
-//const url = 'mongodb://raija:p2assraija@ds245478.mlab.com:45478/hjperson'
+
 
 mongoose.connect(url)
 
